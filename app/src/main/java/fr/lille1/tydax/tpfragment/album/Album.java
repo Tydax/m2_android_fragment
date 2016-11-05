665fc7186392f006ca784d93fe8a4abcac5e57d9
Armand (Tydax) Bour <tydaxx@gmail.com>
2016-11-05 15:19:09 +0100

    Add base Android project

---
+++ app/src/main/java/fr/lille1/tydax/tpfragment/album/Album.java
@@ -0,0 +1,14 @@
+package fr.lille1.tydax.tpfragment.album;
+
+/**
+ * Created by tydax on 04/11/16.
+ */
+
+public class Album {
+
+    protected final String title;
+    protected final String artist;
+    protected final int year;
+    protected final String genre;
+
+}
